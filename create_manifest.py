a3f82c561aebf2fa329cb3fab17200cd79fdbc75
motawie0 <m.mosatafa@nu.edu.eg>
2024-07-25 01:13:12 +0300

    update create_manifest.py

--- create_manifest.py
+++ create_manifest.py
@@ -4,12 +4,11 @@ import librosa
 import argparse
 
 
-def create_manifest(audio_dir, data_dir):
+def create_manifest(audio_dir):
     # Create the output directory if it doesn't exist
-    os.makedirs(data_dir, exist_ok=True)
 
     # Open the output file
-    with open(os.path.join(data_dir, 'input_manifest.json'), 'w') as fp:
+    with open('input_manifest.json', 'w') as fp:
         # Iterate over each audio file in the directory
         for audio_filename in os.listdir(audio_dir):
             if audio_filename.endswith('.wav'):  # or any other audio file extension
@@ -35,8 +34,7 @@ def create_manifest(audio_dir, data_dir):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate a manifest JSON file for a speaker diarization dataset.")
     parser.add_argument('audio_dir', type=str, help='Directory containing the audio files.')
-    parser.add_argument('data_dir', type=str, help='Directory to save the output manifest file.')
 
     args = parser.parse_args()
 
-    create_manifest(args.audio_dir, args.data_dir)
+    create_manifest(args.audio_dir)
